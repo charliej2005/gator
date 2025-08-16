1b0038fe5734bcfc566a40671e852075130c3127
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-16 20:57:06 +0200

    feat: command handle logic

--- main.go
+++ main.go
@@ -17,3 +17,28 @@ func main() {
 	fmt.Printf("dbUrl: %v, username: %v\n", cfg.DbURL, cfg.CurrentUserName)
 	// implement proper functionality later
 }
+
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handler map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	err := c.handler[cmd.name](s, cmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handler[name] = f
+}
