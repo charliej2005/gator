abae3ec45dbb0cec9e53e45aa2e27bbdc89cabaf
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-16 23:38:59 +0200

    feat: users cli command

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func main() {
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerUsers)
 
 	args := os.Args
 	if len(args) < 2 {
@@ -142,6 +143,24 @@ func handlerReset(s *state, cmd command) error {
 	err := s.db.DeleteUsers(context.Background())
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+	if len(users) == 0 {
+		return errors.New("no users found")
+	}
+	for _, user := range users {
+		if user.Name == s.config.CurrentUserName {
+			fmt.Printf("* %v (current)\n", user.Name)
+		} else {
+			fmt.Printf("* %v\n", user.Name)
+		}
 	}
 	return nil
 }
