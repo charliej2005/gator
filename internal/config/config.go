c7b33de34e5239bb76ce0050ae39d3596f27a7cb
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-16 19:18:04 +0200

    feat: write to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -32,6 +32,17 @@ func Read() (Config, error) {
 	return config, nil
 }
 
+func (c *Config) SetUser(username string) error {
+	c.CurrentUserName = username
+
+	err := write(*c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 const configFileName = ".gatorconfig.json"
 
 func getConfigFilePath() (string, error) {
@@ -43,4 +54,24 @@ func getConfigFilePath() (string, error) {
 	configPath := fmt.Sprintf("%v/%v", homePath, configFileName)
 
 	return configPath, nil
+}
+
+func write(cfg Config) error {
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(configPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(cfg); err != nil {
+		return err
+	}
+
+	return nil
 }
