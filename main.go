08f37c2554b822db182a4f2f2c5fc2888c6b4929
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-16 22:34:15 +0200

    feat: register cli command

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/charliej2005/gator/internal/config"
 	"github.com/charliej2005/gator/internal/database"
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
@@ -27,6 +30,7 @@ func main() {
 		handler: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	args := os.Args
 	if len(args) < 2 {
@@ -84,5 +88,42 @@ func handlerLogin(s *state, cmd command) error {
 		return err
 	}
 	fmt.Printf("The user has been set to '%s'\n", username)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("username not provided")
+	}
+	username := cmd.args[0]
+	nullTime := sql.NullTime{Time: time.Now(), Valid: true}
+	userParams := database.CreateUserParams{
+		ID:        uuid.NullUUID{UUID: uuid.New(), Valid: true},
+		CreatedAt: nullTime,
+		UpdatedAt: nullTime,
+		Name:      username,
+	}
+
+	_, err := s.db.GetUser(context.Background(), username)
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+	if err == nil {
+		return errors.New("user with that name already exists")
+	}
+
+	_, err = s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		return err
+	}
+
+	err = s.config.SetUser(username)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("user has been created:\nid: %s\ncreated at: %s\nupdated at: %s\nname: %s\n",
+		userParams.ID.UUID, userParams.CreatedAt.Time, userParams.UpdatedAt.Time, userParams.Name)
+
 	return nil
 }
