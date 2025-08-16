f99b500e6bd489880627d07981b5657feeb2940c
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-16 21:15:17 +0200

    feat: basic cli logic
    CLI arguments passed to program call
    Login command implementation

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/charliej2005/gator/internal/config"
 )
@@ -9,13 +11,28 @@ import (
 func main() {
 	// placeholder main that accesses the config and updates it to my name :)
 	cfg, _ := config.Read()
-	fmt.Printf("dbUrl: %v, username: %v\n", cfg.DbURL, cfg.CurrentUserName)
-	username := "charlie"
-	fmt.Println("setting username")
-	_ = cfg.SetUser(username)
-	cfg, _ = config.Read()
-	fmt.Printf("dbUrl: %v, username: %v\n", cfg.DbURL, cfg.CurrentUserName)
-	// implement proper functionality later
+	s := state{config: &cfg}
+	cmds := commands{
+		handler: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+	args := os.Args
+	if len(args) < 2 {
+		err := errors.New("command not provided")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	userCmdName := args[1]
+	var userArgs []string
+	if len(args) > 2 {
+		userArgs = args[2:]
+	}
+	userCmd := command{name: userCmdName, args: userArgs}
+	err := cmds.run(&s, userCmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 type state struct {
@@ -41,4 +58,17 @@ func (c *commands) run(s *state, cmd command) error {
 
 func (c *commands) register(name string, f func(*state, command) error) {
 	c.handler[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("username not provided")
+	}
+	username := cmd.args[0]
+	err := s.config.SetUser(username)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("The user has been set to '%s'\n", username)
+	return nil
 }
