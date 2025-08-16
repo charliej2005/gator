a2b4b51f93a3c9edf00381be844cc9e8b22b7541
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-16 19:17:01 +0200

    feat: read config

---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DbURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	var config Config
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
+
+const configFileName = ".gatorconfig.json"
+
+func getConfigFilePath() (string, error) {
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	configPath := fmt.Sprintf("%v/%v", homePath, configFileName)
+
+	return configPath, nil
+}
