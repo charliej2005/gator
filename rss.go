acc85d6ba7ff3e19a4d9e3d195a5931e0f766ff1
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-17 12:17:38 +0200

    feat: fetch rss feed

---
+++ rss.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"html"
+	"net/http"
+)
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	resp.Header.Set("User-Agent", "gator")
+
+	var feed RSSFeed
+	decoder := xml.NewDecoder(resp.Body)
+	if err := decoder.Decode(&feed); err != nil {
+		return nil, err
+	}
+
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	for i := range feed.Channel.Item {
+		feed.Channel.Item[i].Title = html.UnescapeString(feed.Channel.Item[i].Title)
+		feed.Channel.Item[i].Description = html.UnescapeString(feed.Channel.Item[i].Description)
+	}
+
+	return &feed, nil
+}
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
